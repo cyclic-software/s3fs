e719103f984192f98ffa3afda5705fab66500e62
Kam Lasater <kam.lasater@cyclic.sh>
2022-12-09 19:27:56 -0500

    fix: added local fs readFileSync

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 console.log('@cyclic.sh/s3fs: index.js')
+const fs = require('fs')
 
 class CyclicS3FS {
   constructor(bucketName) {
     console.log(`new CyclicS3FS('${bucketName}')`)
+  }
+
+  readFileSync(fileName) {
+    return fs.readFileSync(fileName)
   }
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -3,8 +3,13 @@ const s3fs = require("../src")
 
 
 describe("can require", () => {
-  test("test true", async () => {
+  test("test constructor", async () => {
     const fs = new s3fs('test-bucket-name')
     expect(s3fs).toBeDefined()
+  })
+  test("readFileSync()", async () => {
+    const fs = new s3fs('test-bucket-name')
+    const d = fs.readFileSync('test/_read.json')
+    expect(JSON.parse(d)).toEqual({key: 'value'})
   })
 })
