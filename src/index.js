213149278042a064f7297ae335433ce029a20b7b
Kam Lasater <kam.lasater@cyclic.sh>
2022-12-09 21:18:37 -0500

    fix: added async readFile()

--- src/index.js
+++ src/index.js
@@ -1,13 +1,54 @@
-console.log('@cyclic.sh/s3fs: index.js')
 const fs = require('fs')
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+
+
+function streamToBuffer(stream) {
+  return new Promise((resolve, reject) => {
+    const chunks = [];
+    stream.on("data", (chunk) => chunks.push(chunk));
+    stream.on("error", reject);
+    stream.on("end", () => resolve(Buffer.concat(chunks))); // can call .toString("utf8") on the buffer
+  });
+}
 
 class CyclicS3FS {
   constructor(bucketName) {
     console.log(`new CyclicS3FS('${bucketName}')`)
+    this.bucket = bucketName
+    this.s3 = new S3Client({});
+  }
+
+  async readFile(fileName) {
+    const cmd = new GetObjectCommand({
+      Bucket: this.bucket,
+      Key: fileName,
+    })
+
+    let obj = await this.s3.send(cmd)
+    return streamToBuffer(obj.Body)
   }
 
   readFileSync(fileName) {
-    return fs.readFileSync(fileName)
+    var result = null
+    var done = false
+
+    this.readFile(fileName).then((res) => {
+      console.log('then')
+      result = res.Body?.transformToString()
+    }).catch((err) => {
+      console.log('catch')
+      console.error(err)
+      result = {msg: `failed to get ${fileName}`, error: err}
+    }).finally(() => {
+      console.log('finally')
+      done = true
+      result = result || {msg: `unknown error ${fileName}`}
+    })
+    console.log('looping')
+    require('deasync').loopWhile(()=> {return !done})
+    console.log('returning')
+    return result
+    // return fs.readFileSync(fileName)
   }
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,15 +1,24 @@
 
 const s3fs = require("../src")
 
+const bucket = 'cyclic-sh-s3fs-test-bucket-us-east-2'
 
-describe("can require", () => {
+describe("Basic smoke tests", () => {
   test("test constructor", async () => {
-    const fs = new s3fs('test-bucket-name')
+    const fs = new s3fs(bucket)
     expect(s3fs).toBeDefined()
   })
-  test("readFileSync()", async () => {
-    const fs = new s3fs('test-bucket-name')
-    const d = fs.readFileSync('test/_read.json')
-    expect(JSON.parse(d)).toEqual({key: 'value'})
+  test("readFile()", async () => {
+    const fs = new s3fs(bucket)
+    const d = await fs.readFile('test/_read.json')
+    console.log(d)
+    const s = d.toString("utf8")
+    const json = JSON.parse(s)
+    expect(json).toEqual({key: 'value'})
   })
+  // test("readFileSync()", async () => {
+  //   const fs = new s3fs(bucket)
+  //   const d = fs.readFileSync('test/_read.json')
+  //   expect(JSON.parse(d)).toEqual({key: 'value'})
+  // })
 })
