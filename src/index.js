f2fb5169e96aa8929f5603d4a77ea4f65233e8e5
Kam Lasater <kam.lasater@cyclic.sh>
2022-12-09 22:01:38 -0500

    fix: sleep spin on reading from s3

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ function streamToBuffer(stream) {
 
 class CyclicS3FS {
   constructor(bucketName) {
-    console.log(`new CyclicS3FS('${bucketName}')`)
     this.bucket = bucketName
     this.s3 = new S3Client({});
   }
@@ -23,30 +22,44 @@ class CyclicS3FS {
       Bucket: this.bucket,
       Key: fileName,
     })
-
+    console.log('about to send')
     let obj = await this.s3.send(cmd)
+    console.log('send done, read stream')
     return streamToBuffer(obj.Body)
   }
 
   readFileSync(fileName) {
     var result = null
     var done = false
+    var start = Date.now()
+    var timeoutMS = 10 * 1000
 
     this.readFile(fileName).then((res) => {
-      console.log('then')
-      result = res.Body?.transformToString()
-    }).catch((err) => {
-      console.log('catch')
-      console.error(err)
-      result = {msg: `failed to get ${fileName}`, error: err}
-    }).finally(() => {
-      console.log('finally')
+      result = res
       done = true
-      result = result || {msg: `unknown error ${fileName}`}
     })
-    console.log('looping')
-    require('deasync').loopWhile(()=> {return !done})
-    console.log('returning')
+    while(!done && start + timeoutMS > Date.now()) {
+      console.log('spin again')
+      require('deasync').sleep(100);
+    }
+    return result;
+
+
+    // this.readFile(fileName).then((res) => {
+    //   console.log('then')
+    //   result = res
+    //   done = true
+    // }).catch((err) => {
+    //   console.log('catch')
+    //   console.error(err)
+    //   result = {msg: `failed to get ${fileName}`, error: err}
+    // }).finally(() => {
+    //   console.log('finally')
+    //   done = true
+    //   result = result || {msg: `unknown error ${fileName}`}
+    // })
+    // console.log('looping')
+    // require('deasync').loopWhile(()=> {return !done})
     return result
     // return fs.readFileSync(fileName)
   }
--- test/index.test.js
+++ test/index.test.js
@@ -8,17 +8,20 @@ describe("Basic smoke tests", () => {
     const fs = new s3fs(bucket)
     expect(s3fs).toBeDefined()
   })
-  test("readFile()", async () => {
+  // test("readFile()", async () => {
+  //   const fs = new s3fs(bucket)
+  //   const d = await fs.readFile('test/_read.json')
+    // console.log(d)
+    // const s = d.toString("utf8")
+    // const json = JSON.parse(s)
+    // expect(json).toEqual({key: 'value'})
+  // })
+  test("readFileSync()", async () => {
     const fs = new s3fs(bucket)
-    const d = await fs.readFile('test/_read.json')
+    const d = fs.readFileSync('test/_read.json')
     console.log(d)
     const s = d.toString("utf8")
     const json = JSON.parse(s)
     expect(json).toEqual({key: 'value'})
   })
-  // test("readFileSync()", async () => {
-  //   const fs = new s3fs(bucket)
-  //   const d = fs.readFileSync('test/_read.json')
-  //   expect(JSON.parse(d)).toEqual({key: 'value'})
-  // })
 })
